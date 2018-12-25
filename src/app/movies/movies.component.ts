1c1616526976f570304093d22a22564f37341677
Andreas Menge <andi.menge@gmail.com>
2018-12-25 18:28:20 +0100

    add more dummy movies

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -8,6 +8,8 @@ import { Movie } from './movie/movie.model';
 })
 export class MoviesComponent implements OnInit {
   movies: Movie[] = [
+    new Movie('Hellboy', 'https://www.imdb.com/title/tt2274648/', '2019-04-11', false, 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/mpc7oxbiPQ0ICn0EEnaSOUf5Ag5.jpg', '2018-12-23T17:46:13.334Z'),
+    new Movie('Hellboy', 'https://www.imdb.com/title/tt2274648/', '2019-04-11', false, 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/mpc7oxbiPQ0ICn0EEnaSOUf5Ag5.jpg', '2018-12-23T17:46:13.334Z'),
     new Movie('Hellboy', 'https://www.imdb.com/title/tt2274648/', '2019-04-11', false, 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/mpc7oxbiPQ0ICn0EEnaSOUf5Ag5.jpg', '2018-12-23T17:46:13.334Z')
   ];
 
