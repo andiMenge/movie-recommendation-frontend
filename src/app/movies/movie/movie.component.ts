f32deca918377a81cefb4de5aaa211b109625405
Andreas Menge <andi.menge@gmail.com>
2018-12-25 12:44:33 +0100

    add a mock movie object

--- src/app/movies/movie/movie.component.ts
+++ src/app/movies/movie/movie.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from './movie.model';
 
 @Component({
   selector: 'app-movie',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie.component.css']
 })
 export class MovieComponent implements OnInit {
+  mockMovie: Movie = new Movie('Hellboy', 'https://www.imdb.com/title/tt2274648/', '2019-04-11', false, 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/mpc7oxbiPQ0ICn0EEnaSOUf5Ag5.jpg', '2018-12-23T17:46:13.334Z');
 
   constructor() { }
 
