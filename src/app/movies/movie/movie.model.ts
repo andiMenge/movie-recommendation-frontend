10826736a39a2fd94e631769e52ffe9740f4c419
Andreas Menge <andi.menge@gmail.com>
2019-04-05 17:59:23 +0200

    add genres to movie model

--- src/app/movies/movie/movie.model.ts
+++ src/app/movies/movie/movie.model.ts
@@ -5,13 +5,15 @@ export class Movie {
   public is_highlight: boolean;
   public image_url: string;
   public created_date: string;
+  public genres: string[];
 
-  constructor(original_title: string, imdb_id: string, release_date: string, is_highlight: boolean, image_url: string, created_date: string) {
+  constructor(original_title: string, imdb_id: string, release_date: string, is_highlight: boolean, image_url: string, created_date: string, genres: string[]) {
     this.original_title = original_title;
     this.imdb_id = imdb_id;
     this.release_date = release_date;
     this.is_highlight = is_highlight;
     this.image_url = image_url;
     this.created_date = created_date;
+    this.genres = genres;
   }
 }
